248f4dbd09e47704397ec4a9b68d6fec1613ec79
Thomas Juul Andersen <tja_skb@hotmail.com>
2019-01-03 08:44:31 +0100

    updated to lastest version

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,127 +1,49 @@
-var config = {
-    apiKey: "AIzaSyBB5FrvVkgwLiRw0-oBG0s2sd5SlffYeTI",
-    authDomain: "usedtoys-e4d2b.firebaseapp.com",
-    databaseURL: "https://usedtoys-e4d2b.firebaseio.com",
-    projectId: "usedtoys-e4d2b"
- };
-var firebase = firebase.initializeApp(config);
-console.log(firebase.name);
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
 
-$(document).ready(function () {
-   
-    firebase.auth().onAuthStateChanged(function (user) {
-        var user = firebase.auth().currentUser;
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
 
-        if (user) {
-            // $('.views').append("<p>Du er logget ind som:" + user.email + " </p>")
-            // myApp.closeModal('.login-screen');
-            $('html').fadeTo( "fast", 1 );
-
-        } else {
-            // $('.views').append("<p>Du er ikke logget ind</p>")
-            console.log("not logged in");
-            $('.hide-when-logged-in').show();
-            $('html').fadeTo( "fast", 1 );
-            
-        }
-    })
-    $('.close-popup.back').click(function() {
-        // myApp.closeModal('.login-screen');
-    })
-    
-    $('p.sign-up').click(function () {
-        // var provider = new firebase.auth.FacebookAuthProvider();
-
-        email = $("input[name='email']").val();
-        password = $("input[name='password']").val();
-
-        firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then(function (user) {
-            console.log(user.email);
-            // myApp.closeModal('.login-screen');
-            // location.reload();
-        })
-            .catch(function (err) {
-                console.log("register error: " + err);
-            });
-
-        // $.ajax({
-        //     type: 'POST',
-        //     crossDomain: true,
-        //     async: true,
-        //     cache : false,
-        //     data: {
-        //         action: 'createUser',
-        //         username: $( "input[name='username']" ).val(),
-        //         password: $( "input[name='password']" ).val()
-        //     },
-        //     url: "http://jeppejulius.dk/LastChance/index.php",
-        //     dataType: 'JSONc',
-        //     success: function() {
-        //         alert("Worked!!!");
-        //     }
-        // })
-
-    })
-    $('p.log-in').click(function () {
-        console.log("clicked!");
-        // var provider = new firebase.auth.FacebookAuthProvider();
-        // provider.addScope('user_birthday');
-
-        email = $("input[name='email']").val();
-        password = $("input[name='password']").val();
-
-        firebase.auth().signInWithEmailAndPassword(email, password)
-            .then(function (user) {
-                console.log(user.email);
-                // myApp.closeModal('.login-screen');
-                // window.location("/index.html");
-            })
-            .catch(function (err) {
-                console.log("register error: " + err);
-            });
-
-        // $.ajax({
-        //     type: 'POST',
-        //     crossDomain: true,
-        //     async: true,
-        //     cache : false,
-        //     data: {
-        //         action: 'createUser',
-        //         username: $( "input[name='username']" ).val(),
-        //         password: $( "input[name='password']" ).val()
-        //     },
-        //     url: "http://jeppejulius.dk/LastChance/index.php",
-        //     dataType: 'JSONc',
-        //     success: function() {
-        //         alert("Worked!!!");
-        //     }
-        // })
-
-    })
-    $('.tester > div > .log-out').click(function () {
-        firebase.auth().signOut();
-        location.reload();
-        
-    });
-    $('.tester > div > button').click(function () {
-
-        $.ajax({
-            type: 'POST',
-            crossDomain: true,
-            async: true,
-            cache: false,
-            data: {
-                action: 'getUser',
-                username: $("input[name='username']").val(),
-                password: $("input[name='password']").val()
-            },
-            url: "http://jeppejulius.dk/LastChance/index.php",
-            dataType: 'JSONc',
-            success: function () {
-                alert("Worked!!!");
-            }
-        })
-
-    })
-})
+        console.log('Received Event: ' + id);
+    }
+};
