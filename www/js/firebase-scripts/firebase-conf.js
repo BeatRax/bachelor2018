34dd87d1a91c873befa72e928f783242db4553b1
Thomas Juul Andersen <tja_skb@hotmail.com>
2019-01-03 09:24:07 +0100

    new search

--- www/js/firebase-scripts/firebase-conf.js
+++
@@ -1,7 +0,0 @@
-var config = {
-    apiKey: "AIzaSyBB5FrvVkgwLiRw0-oBG0s2sd5SlffYeTI",
-    authDomain: "usedtoys-e4d2b.firebaseapp.com",
-    databaseURL: "https://usedtoys-e4d2b.firebaseio.com",
-    projectId: "usedtoys-e4d2b"
- };
-var firebase = firebase.initializeApp(config);
