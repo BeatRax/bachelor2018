fa6d3c4407291f70f04ef9ef3da106f80250707a
JeppeKrogh <jeppejulius@hotmail.com>
2019-01-13 11:31:41 +0100

    Ændringer så merge virker

--- platforms/browser/www/js/game.js
+++ platforms/browser/www/js/game.js
@@ -2,7 +2,7 @@
 var config = {
     type:Phaser.CANVAS, 
     width:  window.innerWidth,
-    height: window.innerHeight-94,
+    height: window.innerHeight-56,
     parent: 'phaser-container',
     physics: {
         default:'arcade',
